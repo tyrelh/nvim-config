33ec0efba2201e014602cc703461967a17789748
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-14 09:40:06 -0700

    disable lualine in nvim-tree pane

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -42,6 +42,7 @@ return {
         theme = bubbles_theme,
         component_separators = '',
         section_separators = { left = '', right = '' },
+        disabled_filetypes = { 'NvimTree' },
       },
       sections = {
         lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
