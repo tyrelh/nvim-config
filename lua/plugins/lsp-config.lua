7e9c30f8acae613c3b979417eefee08ae1cee432
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-13 22:52:56 -0700

    add bashls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,9 +11,10 @@ return {
     config = function()
       require('mason-lspconfig').setup {
         ensure_installed = {
+          'bashls',
           'lua_ls',
-          'yamlls',
           'tsserver',
+          'yamlls',
         },
       }
     end,
@@ -23,9 +24,10 @@ return {
     'neovim/nvim-lspconfig',
     config = function()
       local lspconfig = require 'lspconfig'
+      lspconfig.bashls.setup {}
       lspconfig.lua_ls.setup {}
-      lspconfig.yamlls.setup {}
       lspconfig.tsserver.setup {}
+      lspconfig.yamlls.setup {}
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Show symbol documentation' })
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = '[g]oto [d]efinition' })
