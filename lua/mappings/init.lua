ca266d49af8410e123dce9e561b49a91bbd223a1
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-16 13:05:41 -0700

    add better keymaps for iterating through open buffers

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -2,6 +2,9 @@
 -- vim.keymap.set('n', '<leader>p', '"+p', { desc = '[p]aste from clipboard' })
 -- vim.keymap.set('n', '<leader>y', '"+y', { desc = '[y]ank to clipboard' })
 
-vim.keymap.set('n', '<leader>t', '<cmd> tabn <CR>', { desc = 'Next [T]ab' })
+-- vim.keymap.set('n', '<leader>t', '<cmd> tabn <CR>', { desc = 'Next [T]ab' })
+
+vim.keymap.set('n', '<S-l>', '<cmd> bnext <CR>', { desc = 'Next Buffer' })
+vim.keymap.set('n', '<S-h>', '<cmd> bprevious <CR>', { desc = 'Previous Buffer' })
 
 return {}
