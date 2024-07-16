19acc3d7bc19fa7111fd35a0ffd5a04188d98765
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-16 13:05:17 -0700

    take out bufferline autocmds that were breaking things

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -296,21 +296,21 @@ return {
       end,
     })
 
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'AlphaReady',
-      desc = 'Disable status and tabline for alpha',
-      callback = function()
-        -- vim.go.laststatus = 0
-        vim.opt.showtabline = 0
-      end,
-    })
-    vim.api.nvim_create_autocmd('BufUnload', {
-      buffer = 0,
-      desc = 'Enable status and tabline after alpha',
-      callback = function()
-        -- vim.go.laststatus = 3
-        vim.opt.showtabline = 1
-      end,
-    })
+    -- vim.api.nvim_create_autocmd('User', {
+    --   pattern = 'AlphaReady',
+    --   desc = 'Disable status and tabline for alpha',
+    --   callback = function()
+    --     -- vim.go.laststatus = 0
+    --     vim.opt.showtabline = 0
+    --   end,
+    -- })
+    -- vim.api.nvim_create_autocmd('BufUnload', {
+    --   buffer = 0,
+    --   desc = 'Enable status and tabline after alpha',
+    --   callback = function()
+    --     -- vim.go.laststatus = 3
+    --     vim.opt.showtabline = 1
+    --   end,
+    -- })
   end,
 }
