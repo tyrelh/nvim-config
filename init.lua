7dd488a54aed8db47a7b214ea589837215e408dc
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-16 12:16:52 -0700

    remove old <leader>ft keymap for nvim tree

--- init.lua
+++ init.lua
@@ -105,8 +105,6 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('n', '<leader>ft', '<cmd> NvimTreeToggle <CR>', { desc = 'Toggle [F]ile [T]ree' })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
