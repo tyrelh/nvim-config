ca0e762757f5e09654b6e26809b9907b44b0ea43
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-16 12:01:36 -0700

    add toggling tabline to new alpha config

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -295,5 +295,22 @@ return {
         vim.cmd 'NvimTreeToggle'
       end,
     })
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'AlphaReady',
+      desc = 'Disable status and tabline for alpha',
+      callback = function()
+        -- vim.go.laststatus = 0
+        vim.opt.showtabline = 0
+      end,
+    })
+    vim.api.nvim_create_autocmd('BufUnload', {
+      buffer = 0,
+      desc = 'Enable status and tabline after alpha',
+      callback = function()
+        -- vim.go.laststatus = 3
+        vim.opt.showtabline = 1
+      end,
+    })
   end,
 }
