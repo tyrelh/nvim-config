c6e641a1af1a0fa02a82aae3179cf8e65b468148
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-17 13:07:49 -0700

    some useful keybinds for pasting and saving

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -10,4 +10,17 @@ vim.keymap.set('i', 'ii', '<ESC>', { desc = 'Exit Insert Mode' })
 vim.keymap.set('n', '<S-l>', '<cmd> bnext <CR>', { desc = 'Next Buffer' })
 vim.keymap.set('n', '<S-h>', '<cmd> bprevious <CR>', { desc = 'Previous Buffer' })
 
+-- Move text
+-- vim.keymap.set('n', '<C-j>', '<cmd> m .+1<CR>==', { desc = 'Move line down' })
+-- vim.keymap.set('n', '<C-k>', '<cmd> m .-2<CR>==', { desc = 'Move line up' })
+-- vim.keymap.set('v', 'J', "<cmd> m '>+1<CR>gv=gv", { desc = 'Move line down' })
+-- vim.keymap.set('v', 'K', "<cmd> m '<-2<CR>gv=gv", { desc = 'Move line up' })
+
+-- past over currently selected text without yanking it
+vim.keymap.set('v', 'p', '"_dp', { desc = 'Paste over' })
+vim.keymap.set('v', 'P', '"_dP', { desc = 'Paste over' })
+
+-- save like you are used to
+vim.keymap.set({ 'i', 'n', 'v', 's' }, '<C-s>', '<cmd>w<CR><esc>', { desc = 'Save' })
+
 return {}
