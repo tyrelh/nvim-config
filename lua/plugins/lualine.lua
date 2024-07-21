541b84f83ef8e4702c155e766511d19f73654855
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-20 22:17:42 -0700

    reworked lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,71 +1,51 @@
--- Bubbles config for lualine
--- Author: lokesh-krishna
--- MIT license, see LICENSE for more details.
-
--- stylua: ignore
-local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#8AADF4',
-  black  = '#25273A',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#CBA6F7',
-  grey   = '#25273A',
-}
-
-local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.white },
-  },
-
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.white },
-  },
-}
-
 return {
   'nvim-lualine/lualine.nvim',
-  config = function()
-    require('lualine').setup {
-      -- options = {
-      --   theme = 'dracula',
-      -- },
-      options = {
-        theme = bubbles_theme,
-        component_separators = '',
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = { 'NvimTree' },
+  opts = {
+    options = {
+      component_separators = '',
+      section_separators = { left = '', right = '' },
+      theme = 'catppuccin',
+      disabled_filetypes = { 'NvimTree' },
+    },
+    sections = {
+      lualine_a = {
+        { 'mode', separator = { left = '' }, right_padding = 2 },
       },
-      sections = {
-        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-        lualine_b = { 'filename', 'branch' },
-        lualine_c = {
-          '%=', --[[ add your center compoentnts here in place of this comment ]]
+      lualine_b = {
+        { 'filetype', icon_only = false, right_padding = 0 },
+        {
+          'filename',
+          symbols = {
+            modified = '',
+            readonly = '󰷤 RO',
+            unnamed = '󰊠', -- unnamed buffers.
+            newfile = '', -- newly created file before first write
+          },
+          left_padding = 0,
         },
-        lualine_x = {},
-        lualine_y = { 'filetype', 'progress' },
-        lualine_z = {
-          { 'location', separator = { right = '' }, left_padding = 2 },
-        },
+        { 'branch', icon = '' },
+        { 'diff', symbols = { added = ' ', modified = ' ', removed = ' ' } },
+        'diagnostics',
       },
-      inactive_sections = {
-        lualine_a = { 'filename' },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = { 'location' },
+      lualine_c = {},
+
+      lualine_x = {},
+      lualine_y = {
+        { 'progress', icon = '' },
       },
-      tabline = {},
-      extensions = {},
-    }
-  end,
+      lualine_z = {
+        { 'location', separator = { right = '' }, left_padding = 0 },
+      },
+    },
+    inactive_sections = {
+      lualine_a = { 'filename' },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { 'location' },
+    },
+    tabline = {},
+    extensions = {},
+  },
 }
