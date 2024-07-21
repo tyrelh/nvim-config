a01069f401a8f8917c5a1ec372ec76112884e7b7
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-20 20:27:13 -0700

    rework alpha with custom lists of recent files

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -6,23 +6,28 @@
 
 math.randomseed(os.time())
 
-local function pick_color()
-  local colors = {
-    'String', -- green
-    -- 'Comment', -- grey
-    'Function', -- blue`
-    'Identifier', -- light peach
-    'Keyword', -- purple
-    -- 'Number', -- orange
-    'Operator', -- cyan
-    -- 'PreProc', -- pink
-    'Special', -- pink
-    'Statement', -- purple
-    -- 'Type', -- yellow
-    -- 'Underlined', -- literally underlined
-  }
-  return colors[math.random(#colors)]
-end
+local colors = {
+  green = 'String',
+  blue = 'Function',
+  peach = 'Identifier',
+  purple = 'Keyword',
+  cyan = 'Operator',
+  pink = 'Special',
+}
+local icons = {
+  ui = {
+    file = '',
+    files = '',
+    open_folder = '',
+    config = '',
+    close = '󰈆',
+    git = '',
+    elipsis = '󰇘',
+    lightning = '⚡',
+    branch = '',
+    tree = '󰙅',
+  },
+}
 
 return {
   'goolord/alpha-nvim',
@@ -35,20 +40,99 @@ return {
     local alpha = require 'alpha'
     local dashboard = require 'alpha.themes.dashboard'
 
-    local icons = {
-      ui = {
-        file = '',
-        files = '',
-        open_folder = '',
-        config = '',
-        close = '󰈆',
-        git = '',
-      },
-    }
+    vim.cmd 'rshada'
+    local filtered_paths = {}
 
-    local function whitespace_only(str)
-      return str:match '^%s*$' ~= nil
+    for _, path in ipairs(vim.v.oldfiles) do
+      if not path:match 'NvimTree_%d+$' then
+        table.insert(filtered_paths, path)
+      end
     end
+
+    local base_directory = vim.fn.getcwd()
+    local cwd_paths = {}
+    local current_cwd_paths_amount = 0
+    local desired_cwd_paths_amount = 5
+    local global_paths = {}
+    local current_global_paths_amount = 0
+    local desired_global_paths_amount = 5
+
+    local function remove_cwd(path)
+      return path:gsub(base_directory .. '/', '')
+    end
+
+    local function shorten_home(path)
+      return path:gsub(vim.env.HOME, '~')
+    end
+
+    -- sort paths into those in the current working directory and those not
+    for _, path in ipairs(filtered_paths) do
+      if string.find(path, base_directory, 1, true) then
+        if current_cwd_paths_amount < desired_cwd_paths_amount then
+          table.insert(cwd_paths, path)
+          current_cwd_paths_amount = current_cwd_paths_amount + 1
+        end
+      else
+        if current_global_paths_amount < desired_global_paths_amount then
+          table.insert(global_paths, path)
+          current_global_paths_amount = current_global_paths_amount + 1
+        end
+      end
+    end
+
+    -- truncate a path, rounding down to the nearest /
+    local function truncate_path(path, max_length)
+      if not max_length then
+        max_length = 50
+      end
+      if #path <= max_length then
+        return path
+      end
+      local truncated_path = path:sub(-max_length)
+      local first_slash_pos = truncated_path:find '/'
+      if first_slash_pos then
+        truncated_path = truncated_path:sub(first_slash_pos)
+      end
+      return icons.ui.elipsis .. truncated_path
+    end
+
+    local function map_path_to_button(paths, keybind_offset, style_func)
+      local buttons = {}
+      for i, path in ipairs(paths) do
+        local path_desc = path
+        if style_func then
+          path_desc = style_func(path)
+        end
+        table.insert(
+          buttons,
+          dashboard.button(tostring(i + keybind_offset - 1), icons.ui.file .. '  ' .. truncate_path(path_desc, 44), function()
+            vim.cmd('e ' .. path)
+          end)
+        )
+      end
+      return buttons
+    end
+
+    local function get_git_repo()
+      local result = vim.fn.system 'git remote get-url origin'
+      if vim.v.shell_error ~= 0 then
+        return ''
+      end
+      -- Extract the repo name from the URL
+      local repo_name = result:match '.*/(.*).git'
+      return icons.ui.git .. '  ' .. repo_name
+    end
+
+    local function get_git_branch()
+      local result = vim.fn.system 'git branch --show-current'
+      if vim.v.shell_error ~= 0 then
+        return ''
+      end
+      return icons.ui.branch .. '  ' .. result:gsub('%s+', '')
+    end
+
+    local repo_name = get_git_repo()
+    local branch_name = get_git_branch()
 
     local function pad(n)
       return { type = 'padding', val = n }
@@ -68,41 +152,6 @@ return {
       return lines
     end
 
-    local function format_git_header()
-      local git_dashboard_raw = require('git-dashboard-nvim').setup {
-        days = { 's', 'm', 't', 'w', 't', 'f', 's' },
-        -- how_contributions_count = false,
-        -- empty_square = ' ',
-        -- filled_squares = { '', '', '', '', '', '' },
-        -- show_only_weeks_with_commits = true,
-      }
-      local git_dashboard = {}
-      for _, line in ipairs(git_dashboard_raw) do
-        if not whitespace_only(line) then
-          table.insert(git_dashboard, line)
-        end
-      end
-
-      local git_repo = git_dashboard[1]
-      local git_branch = git_dashboard[#git_dashboard]
-
-      if git_repo == nil and git_branch == nil then
-        return {
-          type = 'text',
-          val = '',
-          opts = { position = 'center' },
-        }, {}
-      end
-
-      local git_branch_section = {
-        type = 'text',
-        val = ' ' .. git_repo .. ':' .. string.sub(git_branch, 5, #git_branch),
-        opts = { position = 'center', hl = pick_color() },
-      }
-
-      return git_branch_section, { unpack(git_dashboard, 2, #git_dashboard - 1) }
-    end
-
     local ascii_header = [[
                                                                    
       ████ ██████           █████      ██                    
@@ -112,30 +161,66 @@ return {
    █████████ ██████████ █████████ █████ █████ ████ █████  
  ███████████ ███    ███ █████████ █████ █████ ████ █████ 
 ██████  █████████████████████ ████ █████ █████ ████ ██████
-                                                                     
     ]]
-    local git_branch_section, commit_history = format_git_header()
 
-    for _, line in ipairs(commit_history) do
-      ascii_header = ascii_header .. '\n' .. string.rep(' ', 3) .. line
-    end
+    local header = {
+      type = 'text',
+      val = center_header(ascii_header),
+      opts = { hl = colors.blue },
+    }
 
-    local custom_header = center_header(ascii_header)
-    local c = pick_color()
-    local header = { type = 'text', val = custom_header, opts = { hl = c } }
+    local git_section = {
+      type = 'text',
+      val = repo_name .. '  ' .. branch_name,
+      opts = { position = 'center', hl = colors.purple },
+    }
 
-    local buttons = {
+    local cwd_section = {
+      type = 'text',
+      val = icons.ui.open_folder .. '  ' .. base_directory,
+      opts = { position = 'center', hl = colors.pink },
+    }
+
+    local recent_cwd_files_section_header = {
+      type = 'text',
+      val = icons.ui.files .. '  Recent CWD Files                               ', -- these spaces are to aligh the text with section below
+      opts = { position = 'center', hl = colors.green },
+    }
+    local recent_cwd_files_section = {
+      type = 'group',
+      val = map_path_to_button(cwd_paths, 0, remove_cwd),
+      opts = {},
+    }
+
+    local recent_global_files_section_header = {
+      type = 'text',
+      val = icons.ui.files .. '  Recent Files                                   ', -- these spaces are to aligh the text with section below
+      opts = { position = 'center', hl = colors.blue },
+    }
+    local recent_global_files_section = {
+      type = 'group',
+      val = map_path_to_button(global_paths, 5, shorten_home),
+      opts = {},
+    }
+
+    local actions_section_header = {
+      type = 'text',
+      val = icons.ui.lightning .. ' Actions                                         ', -- these spaces are to aligh the text with section below
+      opts = { position = 'center', hl = colors.purple },
+    }
+    local actions_section = {
       type = 'group',
       val = {
-        dashboard.button('e', icons.ui.file .. '  New file', '<cmd>new<CR>'),
-        dashboard.button('o', icons.ui.files .. '  Recent Files', '<cmd>Telescope oldfiles<cr>'),
+        dashboard.button('f', icons.ui.tree .. '  File Tree', '<cmd>NvimTreeToggle<CR>'),
+        -- dashboard.button('e', icons.ui.file .. '  New file', '<cmd>new<CR>'),
+        -- dashboard.button('r', icons.ui.files .. '  Recent Files', '<cmd>Telescope oldfiles<cr>'),
         -- dashboard.button('f', icons.ui.open_folder .. '  Explorer', '<cmd>Oil<cr>'),
         -- dashboard.button('c', icons.ui.config .. '  Neovim config', '<cmd>Oil /home/cafebabe/install/dotfiles/config/nvim<cr>'),
         -- dashboard.button('g', icons.ui.git .. '  Open Git', '<cmd>Neogit<CR>'),
-        dashboard.button('l', '󰒲  Lazy', '<cmd>Lazy<cr>'),
+        -- dashboard.button('l', '󰒲  Lazy', '<cmd>Lazy<cr>'),
         dashboard.button('q', icons.ui.close .. '  Quit NVIM', ':qa<CR>'),
       },
-      opts = {},
+      opts = { position = 'center' },
     }
 
     -- Custom footer showing number of plugins loaded
@@ -156,31 +241,32 @@ return {
     -- Setting up the alpha layout
     alpha.setup {
       layout = {
-        pad(4),
         header,
+        git_section,
+        cwd_section,
         pad(1),
-        git_branch_section,
+        recent_cwd_files_section_header,
+        recent_cwd_files_section,
         pad(1),
-        buttons,
-        -- pad(1),
-        -- bottom_section,
-        -- pad(1),
-        -- footer,
+        recent_global_files_section_header,
+        recent_global_files_section,
+        pad(1),
+        actions_section_header,
+        actions_section,
       },
     }
 
-    -- NOTE: This gets nvim-tree to open on startup, and thus is an implicit dependency
-    vim.api.nvim_create_autocmd('User', {
-      once = true,
-      pattern = 'LazyVimStarted',
-      desc = 'Open NvimTree after startup',
-      callback = function()
-        vim.cmd 'NvimTreeToggle'
-        vim.cmd(vim.api.nvim_replace_termcodes('normal <C-h>', true, true, true))
-      end,
-    })
+    -- vim.api.nvim_create_autocmd('User', {
+    --   once = true,
+    --   pattern = 'LazyVimStarted',
+    --   desc = 'Open NvimTree after startup',
+    --   callback = function()
+    --     vim.cmd 'NvimTreeToggle'
+    --     vim.cmd(vim.api.nvim_replace_termcodes('normal <C-h>', true, true, true))
+    --   end,
+    -- })
 
-    -- NOTE: these were to get statusline to not appear on alpha, but they're not working correctly
+    -- these were to get statusline to not appear on alpha, but they're not working correctly
     -- vim.api.nvim_create_autocmd('User', {
     --   pattern = 'AlphaReady',
     --   desc = 'Disable status and tabline for alpha',
