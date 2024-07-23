34ec7298a134511eb10e7da2981429b1e1427097
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-23 10:23:59 -0700

    experimenting with auto-session

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,15 @@
+return {}
+-- https://github.com/rmagatti/auto-session
+-- return {
+--   {
+--     'rmagatti/auto-session',
+--     dependencies = {
+--       'nvim-telescope/telescope.nvim', -- Only needed if you want to use sesssion lens
+--     },
+--     config = function()
+--       require('auto-session').setup {
+--         auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+--       }
+--     end,
+--   },
+-- }
