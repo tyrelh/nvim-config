922cdbb942e3ac795302cf9f8e21f4ec18915262
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-23 20:32:47 -0700

    reconfigure lualine, install copilot lualine extension

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,53 +1,69 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  opts = {
-    options = {
-      component_separators = '',
-      section_separators = { left = '', right = '' },
-      theme = 'catppuccin',
-      -- disabled_filetypes = { 'NvimTree' },
-    },
-    sections = {
-      lualine_a = {
-        { 'mode', separator = { left = '' }, right_padding = 2 },
+  {
+    'AndreM222/copilot-lualine',
+    dependencies = { 'zbirenbaum/copilot.lua' },
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        component_separators = '',
+        section_separators = { left = '', right = '' },
+        theme = 'catppuccin',
+        -- disabled_filetypes = { 'NvimTree' },
       },
-      lualine_b = {
-        { 'filetype', icon_only = false, right_padding = 0 },
-        {
-          'filename',
-          symbols = {
-            modified = '',
-            readonly = '󰷤',
-            unnamed = '󰊠', -- unnamed buffers.
-            newfile = '', -- newly created file before first write
-          },
-          left_padding = 0,
+      sections = {
+        lualine_a = {
+          { 'mode', separator = { left = '', right = '' } },
         },
-        { 'branch', icon = '' },
-        { 'diff', symbols = { added = ' ', modified = ' ', removed = ' ' } },
-        'diagnostics',
-      },
-      lualine_c = {},
+        lualine_b = {
+          { 'location', left_padding = 0 },
+          { 'progress', icon = '' },
+          { 'filetype', icon_only = false, right_padding = 0 },
+          {
+            'filename',
+            symbols = {
+              modified = '',
+              readonly = '󰷤',
+              unnamed = '󰊠', -- unnamed buffers.
+              newfile = '', -- newly created file before first write
+            },
+            left_padding = 0,
+          },
+        },
+        lualine_c = {},
 
-      lualine_x = {},
-      lualine_y = {
-        { 'progress', icon = '' },
+        lualine_x = {},
+        lualine_y = {
+          'diagnostics',
+          {
+            'diff',
+            symbols = {
+              added = ' ',
+              modified = ' ',
+              removed = ' ',
+            },
+          },
+          { 'branch', icon = '' },
+        },
+        lualine_z = {
+          { 'copilot', separator = { left = '', right = '' } },
+        },
       },
-      lualine_z = {
-        { 'location', separator = { right = '' }, left_padding = 0 },
+      inactive_sections = {
+        lualine_a = { 'location', 'filename' },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {
+          { 'branch', icon = '' },
+        },
+        lualine_z = {},
       },
-    },
-    inactive_sections = {
-      lualine_a = { 'filename' },
-      lualine_b = {},
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = { 'location' },
-    },
-    tabline = {},
-    extensions = {
-      'nvim-tree',
+      tabline = {},
+      extensions = {
+        'nvim-tree',
+      },
     },
   },
 }
