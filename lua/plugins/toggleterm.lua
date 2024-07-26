b2e879d9cc178a6fbe819ed72ff9dc0bede8c2a9
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-25 22:02:00 -0700

    install toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+  'akinsho/toggleterm.nvim',
+
+  version = '*',
+  opts = {
+    open_mapping = [[<c-\>]],
+    clost_on_exit = true,
+    direction = 'float',
+    -- float_ops = {
+    --   border = 'shadow',
+    -- },
+  },
+}
