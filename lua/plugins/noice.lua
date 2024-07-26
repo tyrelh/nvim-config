c43f83e3180fe798aff277263a70aaf8da14e560
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-26 09:42:21 -0700

    increase padding on floating command line

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,6 +1,5 @@
 return {
   -- https://github.com/folke/noice.nvim
-  -- Clean floating command line
   -- Taken from https://www.youtube.com/watch?v=upM6FOtdLeU
   -- That video also talks about a comand cmp plugin to give you autocompletes
   -- for comamnds and searches
@@ -13,13 +12,15 @@ return {
         view = 'notify',
         filter = { event = 'msg_showmode' },
       } },
+      -- clean command line
+      views = {
+        cmdline_popup = {
+          border = {
+            -- style = 'shadow',
+            padding = { 1, 2 },
+          },
+        },
       },
-      -- messages = {
-      -- enabled = false,
-      -- },
-      -- notify = {
-      -- enabled = false,
-      -- },
     },
     dependencies = {
       'MunifTanjim/nui.nvim', -- https://github.com/rcarriga/nvim-notify?tab=readme-ov-file#configuration
