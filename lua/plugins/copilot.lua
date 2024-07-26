57a35e12b76338cc70990229e8cc2112e63235ae
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-25 22:02:48 -0700

    install copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,18 +1,36 @@
 return {
-  'zbirenbaum/copilot.lua',
-  cmd = 'Copilot',
-  event = 'InsertEnter',
-  opts = {
-    panel = {
-      enabled = false,
-      auto_trigger = false,
-    },
-    suggestion = {
-      enabled = true,
-      auto_trigger = true,
-      keymap = {
-        accept = '<C-l>',
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    opts = {
+      panel = {
+        enabled = false,
+        auto_trigger = false,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = '<C-l>',
+        },
       },
     },
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    setup = function()
+      vim.keymap.set('n', '<leader>cc', '<cmd>CopilotChatOpen<cr>', { noremap = true, silent = true, desc = 'Open [C]opilot [C]hat' })
+    end,
+    -- See Commands section for default commands if you want to lazy load on them
   },
 }
