076683af63f810c7ca7e3f8dd9e97d9e235fe325
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-07-27 14:08:16 -0700

    remove note highlight in comment

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -15,7 +15,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
 --
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
