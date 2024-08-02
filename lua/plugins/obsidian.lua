adc01a80593037f3ac92ac562abfa25172d730bb
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-08-01 22:50:48 -0700

    pull a bunch more default config from the plugin repo

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -64,6 +64,63 @@ return {
       folder = 'templates/insertable',
       date_format = '%Y-%m-%d',
     },
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      local suffix = ''
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(suffix)
+    end,
+
+    -- Optional, customize how note file names are generated given the ID, target directory, and title.
+    ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+    ---@return string|obsidian.Path The full path to the new note.
+    note_path_func = function(spec)
+      -- This is equivalent to the default behavior.
+      local path = spec.dir / tostring(spec.id)
+      return path:with_suffix '.md'
+    end,
+
+    -- Optional, customize how wiki links are formatted. You can set this to one of:
+    --  * "use_alias_only", e.g. '[[Foo Bar]]'
+    --  * "prepend_note_id", e.g. '[[foo-bar|Foo Bar]]'
+    --  * "prepend_note_path", e.g. '[[foo-bar.md|Foo Bar]]'
+    --  * "use_path_only", e.g. '[[foo-bar.md]]'
+    -- Or you can set it to a function that takes a table of options and returns a string, like this:
+    wiki_link_func = 'use_alias_only',
+    preferred_link_style = 'wiki',
+
+    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+    -- URL it will be ignored but you can customize this behavior here.
+    ---@param url string
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      vim.fn.jobstart { 'open', url } -- Mac OS
+      -- vim.fn.jobstart({"xdg-open", url})  -- linux
+      -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+      -- vim.ui.open(url) -- need Neovim 0.10.0+
+    end,
+
+    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an image
+    -- file it will be ignored but you can customize this behavior here.
+    ---@param img string
+    follow_img_func = function(img)
+      vim.fn.jobstart { 'qlmanage', '-p', img } -- Mac OS quick look preview
+      -- vim.fn.jobstart({"xdg-open", url})  -- linux
+      -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+    end,
+
+    open_app_foreground = true,
+
     -- Optional, configure additional syntax highlighting / extmarks.
     -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
     ui = {
@@ -107,6 +164,31 @@ return {
         ObsidianBlockID = { italic = true, fg = '#89ddff' },
         ObsidianHighlightText = { bg = '#75662e' },
       },
+    },
+    -- Specify how to handle attachments.
+    attachments = {
+      -- The default folder to place images in via `:ObsidianPasteImg`.
+      -- If this is a relative path it will be interpreted as relative to the vault root.
+      -- You can always override this per image by passing a full path to the command instead of just a filename.
+      img_folder = 'images', -- This is the default
+
+      -- Optional, customize the default name or prefix when pasting images via `:ObsidianPasteImg`.
+      ---@return string
+      img_name_func = function()
+        -- Prefix image names with timestamp.
+        return string.format('%s-', os.time())
+      end,
+
+      -- A function that determines the text to insert in the note when pasting an image.
+      -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+      -- This is the default implementation.
+      ---@param client obsidian.Client
+      ---@param path obsidian.Path the absolute path to the image file
+      ---@return string
+      img_text_func = function(client, path)
+        path = client:vault_relative_path(path) or path
+        return string.format('![%s](%s)', path.name, path)
+      end,
     },
   },
 }
