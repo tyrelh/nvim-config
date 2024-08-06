0a67a3e30ee89b13574d9f883159569c85cad026
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-08-06 14:58:43 -0700

    use markdown plugin for markdown syntax highlights

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,7 @@
+-- https://github.com/MeanderingProgrammer/markdown.nvim
+return {
+  'MeanderingProgrammer/markdown.nvim',
+  main = 'render-markdown',
+  opts = {},
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+}
